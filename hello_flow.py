6d697cbe5746a6b9ce5beb5cce841ee5b96863a4
tamilarasanm676-crypto <tamilarasanm676@gmail.com>
2025-12-11 15:37:23 +0530

    Fix indentation and clean up hello_flow.py

--- hello_flow.py
+++ hello_flow.py
@@ -1,12 +1,8 @@
 from prefect import flow
 
 @flow
-
 def hello_world():
-
-print("Hello from GitHub!")
-
-
+  print("Hello from GitHub!")
+  
 if __name__ == "__main__":
-
-hello_world()
+  hello_world()
