fc0214418869b1ec34549c2d7ca264b7d77dca4c
tamilarasanm676-crypto <tamilarasanm676@gmail.com>
2025-12-12 11:49:24 +0530

    Create batch_job.py

---
+++ batch_job.py
@@ -0,0 +1,38 @@
+from prefect import flow, task
+
+
+@task
+
+def extract():
+
+return "Data Extracted"
+
+
+@task
+
+def transform(data):
+
+return f"{data} → Transformed"
+
+
+@task
+
+def load(data):
+
+print(f"Loading: {data}")
+
+
+@flow
+
+def etl_flow():
+
+raw = extract()
+
+processed = transform(raw)
+
+load(processed)
+
+
+if __name__ == "__main__":
+
+etl_flow()
