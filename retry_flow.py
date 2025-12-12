468ed50d90c8ad98abb76ded305dbfbd66ca98a9
tamilarasanm676-crypto <tamilarasanm676@gmail.com>
2025-12-12 16:08:44 +0530

    Update retry_flow.py

--- retry_flow.py
+++ retry_flow.py
@@ -3,11 +3,14 @@ from random import randint
 
 @task(retries=3, retry_delay_seconds=2)
 def unstable_task():
-  n = randint(1, 10)
-  if n <= 7:
-    raise Exception("Random failure!")
-    print("Success on attempt")
+    n = randint(1, 10)
+    if n <= 7:
+        raise Exception("Random failure!")
+    else:
+        print(f"Success! Random number was {n}")
+        return n
 
 @flow
 def retry_example():
-  unstable_task()
+    result = unstable_task()
+    print("Final result:", result)
