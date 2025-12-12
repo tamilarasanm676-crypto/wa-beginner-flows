11aac1282215a26bd8a64d07b48e7016c8a9d378
tamilarasanm676-crypto <tamilarasanm676@gmail.com>
2025-12-12 12:48:01 +0530

    Implement retry mechanism for unstable task

---
+++ retry_flow.py
@@ -0,0 +1,11 @@
+@task(retries=3, retry_delay_seconds=2)
+def unstable_task():
+  n = randint(1, 10)
+if n <= 7:
+  raise Exception("Random failure!")
+  print("Success on attempt")
+
+
+@flow
+def retry_example():
+  unstable_task()
